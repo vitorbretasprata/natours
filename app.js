31e35ca1087c6c4c54ecce3d4bdf41220245d4b3
Vitor Prata <39749209+vitorbretasprata@users.noreply.github.com>
2021-05-04 14:32:33 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const port = 3000;
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: { tours }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Port is listening on ${port}...`)
+})
