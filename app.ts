020acd66db7c4d926933d7d997228f76f45633e7
Vitor Prata <39749209+vitorbretasprata@users.noreply.github.com>
2021-05-04 16:49:49 -0300

    Add post

--- app.ts
+++ app.ts
@@ -4,6 +4,8 @@ import fs from "fs";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`).toString()
 );
@@ -14,6 +16,25 @@ app.get("/api/v1/tours", (req, res) => {
         results: tours.length,
         data: { tours }
     });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours), 
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour
+                }
+            });
+        });
 });
 
 app.listen(port, () => {
