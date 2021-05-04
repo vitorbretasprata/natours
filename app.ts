72c95139b91b1d7fe9b3c980969786ea4567a386
Vitor Prata <39749209+vitorbretasprata@users.noreply.github.com>
2021-05-04 17:42:34 -0300

    Add patch and delete

--- app.ts
+++ app.ts
@@ -18,6 +18,23 @@ app.get("/api/v1/tours", (req, res) => {
     });
 });
 
+app.get("/api/v1/tours/:id", (req, res) => {
+    const id = parseInt(req.params.id, 10);
+
+    const tour = tours.find((t : any) => t.id === id);
+
+    if(!tour) {
+        res.status(404).json({
+            status: 'Not Found'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: { tour }
+    });
+});
+
 app.post("/api/v1/tours", (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body);
@@ -35,6 +52,58 @@ app.post("/api/v1/tours", (req, res) => {
                 }
             });
         });
+});
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+    const id = parseInt(req.params.id, 10);
+
+    if(id > tours.length) {
+        res.status(500).json({
+            status: 'Failed',
+            message: "Invalid ID"
+        });
+    }
+
+    const tour = tours.find((t : any) => t.id === id);
+
+    if(!tour) {
+        res.status(404).json({
+            status: 'Not Found'
+        });
+    }
+
+    res.status(201).json({
+        status: 'success',
+        data: {
+            tour: tour
+        }
+    });
+
+});
+
+app.delete("/api/v1/tours/:id", (req, res) => {
+    const id = parseInt(req.params.id, 10);
+
+    if(id > tours.length) {
+        res.status(500).json({
+            status: 'Failed',
+            message: "Invalid ID"
+        });
+    }
+
+    const tour = tours.find((t : any) => t.id === id);
+
+    if(!tour) {
+        res.status(404).json({
+            status: 'Not Found'
+        });
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+
 });
 
 app.listen(port, () => {
