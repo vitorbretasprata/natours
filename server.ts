629bcd4fec2b7f1fa255e4907dab40b5949b6def
Vitor Prata <39749209+vitorbretasprata@users.noreply.github.com>
2021-05-06 14:29:45 -0300

    add tour schema

--- server.ts
+++ server.ts
@@ -1,5 +1,19 @@
 const port = process.env.PORT || 3000;
 import app from "./app";
+import mongoose from "mongoose";
+
+const DB = process.env.CONN_STR.replace(
+    "<PASSWORD>",
+    process.env.DB_PASS
+);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(conn => {
+    console.log(conn.connection);
+});
 
 app.listen(port, () => {
     console.log(`Port is listening on ${port}...`)
