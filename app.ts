458af8eb4e8c4cf6d0ae9243ee15faa4235deb04
Vitor Prata <39749209+vitorbretasprata@users.noreply.github.com>
2021-05-08 21:52:57 -0300

    Add error handlers

--- app.ts
+++ app.ts
@@ -5,10 +5,16 @@ import morgan from "morgan";
 import tourRouter from "./src/routes/tour";
 import userRouter from "./src/routes/user";
 
+import AppError from "./src/helpers/appError";
+import ErrorController from "./src/controllers/error";
+
 const app = express();
+const globalError = new ErrorController();
+
 dotenv.config();
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`))
 
 if(process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
@@ -18,5 +24,11 @@ app.use(express.static(`${__dirname}/public`))
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalError.globalErrorHandler)
 
 export default app;
---
+++ src/controllers/error.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response, ErrorRequestHandler } from 'express';
+
+export default class ErrorController  {
+
+    public globalErrorHandler(err : { [key: string] : any }, req : Request, res : Response, next : NextFunction) {
+
+        err.statusCode = err.statusCode || 500;
+        err.status = err.status || 'error';
+    
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    }
+}
