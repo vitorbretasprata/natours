c92542edb1bc83d8111cf7c931ea7892ce6ba3a1
Vitor Prata <39749209+vitorbretasprata@users.noreply.github.com>
2021-05-12 23:52:11 -0300

    Expanding models

--- src/model/tour.ts
+++ src/model/tour.ts
@@ -73,7 +73,30 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    startLocation: {
+        type: {
+            type: String,
+            default: 'Point',
+            enum: ['Point']
+        },
+        coordinates: [Number],
+        address: String,
+        description: String
+    },
+    locations: [
+        {
+            type: {
+                type: String,
+                default: 'Point',
+                enum: ['Point']
+            },
+            coordinates: [Number],
+            address: String,
+            description: String,
+            day: Number
+        }
+    ]
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
